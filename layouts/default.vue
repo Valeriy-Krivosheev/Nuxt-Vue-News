a22cd7a9fcca4841f56de2e028f01a6ade1cf197
vkrivosheev <krivosheevvv@expocentr.ru>
2024-10-04 12:44:04 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <BaseHeader/>
+    <slot/>
+    <BaseFooter/>
+  </div>
+</template>
+<script setup lang="ts">
+import BaseHeader from "~/components/base/BaseHeader.vue";
+import BaseFooter from "~/components/base/BaseFooter.vue";
+</script>
